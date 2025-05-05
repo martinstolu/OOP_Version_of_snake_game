c1f162b2699145a0c1ab3c6ad5741908ff66991f
Tolu Martins <tolu.martins.a@gmail.com>
2025-05-05 03:20:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ while game_on:
     # Todo 6. Figure out when the game should end
     #        Detecting collision with wall
     if (snake.head.xcor() > 280 or snake.head.xcor() < -280
-        or snake.head.ycor() > 280 or snake.head.ycor() > 280):
+        or snake.head.ycor() > 280 or snake.head.ycor() < -280):
         game_on = False
         score_board.game_over()
 
